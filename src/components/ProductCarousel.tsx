a2f0f14772631bf6ce6cacd0ef54aa1fc09b17a8
cristiansk8 <cdpsk8@gmail.com>
2025-04-19 17:33:10 -0500

    ajustes custom

--- src/components/ProductCarousel.tsx
+++ src/components/ProductCarousel.tsx
@@ -61,68 +61,66 @@ const ProductCarousel: React.FC<ProductCarouselProps> = ({ products }) => {
   );
 
   return (
-<div className="container mx-auto px-4 py-8">
-  <Swiper
-    spaceBetween={20}
-    slidesPerView={4}
-    loop={true}
-    autoplay={{
-      delay: 0,
-      disableOnInteraction: false,
-    }}
-    speed={5000}
-    freeMode={true}
-    grabCursor={true}
-    pagination={{ clickable: true }}
-    navigation={true}
-    modules={[Autoplay, Pagination, Navigation]}
-    breakpoints={{
-      320: { slidesPerView: 2 },
-      640: { slidesPerView: 2 },
-      1024: { slidesPerView: 3 },
-      1280: { slidesPerView: 4 },
-    }}
-    className="!overflow-visible"
-  >
-    {filteredProducts.map((product) => (
-      <SwiperSlide key={product.id} className="!h-auto">
-        <div className="border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col h-full">
-          <div className="relative h-72 w-full">
-            <Image
-              src={product.images[0].src}
-              alt={product.name}
-              fill
-              style={{ objectFit: "cover" }}
-              className="hover:scale-105 transition-transform duration-300"
-              sizes="(max-width: 768px) 100vw, 33vw"
-            />
-          </div>
+    <div className="container mx-auto px-4 py-8 overflow-hidden">
+      <Swiper
+        spaceBetween={20}
+        slidesPerView={4}
+        loop={true}
+        autoplay={{
+          delay: 0,
+          disableOnInteraction: false,
+        }}
+        speed={5000}
+        freeMode={true}
+        grabCursor={true}
+        pagination={{ clickable: true }}
+        modules={[Autoplay, Pagination, Navigation]}
+        breakpoints={{
+          320: { slidesPerView: 2 },
+          640: { slidesPerView: 2 },
+          1024: { slidesPerView: 3 },
+          1280: { slidesPerView: 4 },
+        }}
+        className="!overflow-visible"
+      >
+        {filteredProducts.map((product) => (
+          <SwiperSlide key={product.id} className="!h-auto">
+            <div className="border rounded-lg overflow-hidden shadow-lg hover:shadow-xl transition-shadow duration-300 flex flex-col h-full">
+              <div className="relative h-72 w-full">
+                <Image
+                  src={product.images[0].src}
+                  alt={product.name}
+                  fill
+                  style={{ objectFit: "cover" }}
+                  className="hover:scale-105 transition-transform duration-300"
+                  sizes="(max-width: 768px) 100vw, 33vw"
+                />
+              </div>
 
-          <div className="p-4 flex flex-col flex-grow">
-            <h3 className="text-lg font-semibold mb-4">{product.name}</h3>
+              <div className="p-4 flex flex-col flex-grow">
+                <h3 className="text-lg font-semibold mb-4">{product.name}</h3>
 
-            <div className="mt-auto">
-              <a
-                href={generateWhatsAppLink(product.name)}
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{
-                  backgroundColor: colorWha, // Color de fondo principal
-                  color: colorText, // Color del texto
-                }}
-                className="block w-full text-center py-2 px-4 rounded-md transition-all duration-300 hover:bg-[#25d366] hover:text-white"
-              >
-                Comprar
-              </a>
+                <div className="mt-auto">
+                  <a
+                    href={generateWhatsAppLink(product.name)}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    style={{
+                      backgroundColor: colorWha, // Color de fondo principal
+                      color: colorText, // Color del texto
+                    }}
+                    className="whatsapp-button block w-full text-center py-2 px-4 rounded-md transition-all duration-300 hover:bg-[#25d366] hover:text-white"
+                  >
+                    Comprar
+                  </a>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-      </SwiperSlide>
-    ))}
-  </Swiper>
-</div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
 
-  
   );
 };
 
